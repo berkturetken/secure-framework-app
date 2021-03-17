658dc360051a6bd23ff33f1e57defc4f36da5412
Berk Türetken <berkturetken1997@hotmail.com>
2021-03-17 19:06:40 +0300

    Renewed home screen and implemented message sending securely

--- lib/repository/encryptionRepo.dart
+++ lib/repository/encryptionRepo.dart
@@ -45,3 +45,25 @@ Future<void> encryptAndSend(String plainText) async {
   }
   // return responseJson;
 }
+
+
+Future<Map> sendMessage(String message, String email) async {
+  var responseJson;
+  final body = {
+    "message": message,
+    "email": email
+  };
+  final jsonString = json.encode(body);
+  
+  try {
+    final response = await http.post(
+      'https://h24q9fa19h.execute-api.eu-central-1.amazonaws.com/test/light-toggle',
+      body: jsonString
+    );
+    responseJson = jsonDecode(response.body);
+    print(responseJson);
+  } on Exception catch(_) {
+    print("Exception occurs in sendMessage() - encryptionRepo ...");
+  }
+  return responseJson;
+}
--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -1,6 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:secure_framework_app/components/defaultButton.dart';
+import 'package:secure_framework_app/components/constants.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
 
+import 'package:secure_framework_app/repository/encryptionRepo.dart';
+
+Future<void> encryptMessage(String message) async {
+  final storage = Storage;
+  String masterKey = await storage.read(key: "masterKey");
+
+  var message_in_bytes = utf8.encode(message);
+
+  // Encode the key and IV
+  var encodedKey = base64.encode(utf8.encode(masterKey.substring(0, 32)));
+  var encodedIV = base64.encode(utf8.encode(masterKey.substring(32, 48)));
+  var hmacKey = utf8.encode(masterKey.substring(48, 80));
+
+  // Determine key and IV
+  final key = encrypt.Key.fromBase64(encodedKey);
+  final iv = encrypt.IV.fromBase64(encodedIV);
+
+  // Create an encrypter with the key and set the mode as CBC (Cipher Block Chaining)
+  final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
+
+  final encrypted = encrypter.encrypt(message, iv: iv);
+  print("Encrypted Message: " + encrypted.base64);
+  // print(encrypted.base64);
+
+  var hmacSha256 = Hmac(sha256, hmacKey);                 // HMAC-SHA256
+  var digest = hmacSha256.convert(message_in_bytes);
+  var hmac = base64.encode(digest.bytes);
+  print("HMAC: " + hmac);
+  print("Length: " + hmac.length.toString());
+
+  var data = encrypted.base64 + hmac;
+  print("Sent data: " + data);
+  
+  // Sending the light message and waiting for response
+  Map jsonResponse = await sendMessage(data, "phil@gmail.com");
+  var response = jsonResponse["body"];
+  print("Response: " + response);
+}
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -30,7 +72,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     text: "Encrypt",
                     buttonType: "Orange",
                     press: () {
-                      print("Deneme");
+                      var message = {
+                        "light": 1,
+                      };
+                      var formattedMessage = json.encode(message);
+                      encryptMessage(formattedMessage);
                     },
                   )
                 ],
